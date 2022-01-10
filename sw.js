2e6c7d32de953ee6662fee49f1e2b61d56007e92
codelive <dongfa@yeah.net>
2022-01-10 15:09:05 +0800

    add face

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220110.1227';
+const cacheName = 'chirpy-20220110.1431';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
