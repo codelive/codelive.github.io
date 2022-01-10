f0341d38dc81002ec9c5d854a9f6e1dc3f388a24
codelive <dongfa@yeah.net>
2022-01-10 16:20:55 +0800

    change icons

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220110.1553';
+const cacheName = 'chirpy-20220110.1620';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
