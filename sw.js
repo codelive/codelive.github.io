45afe960047f52757989458d6cb65d90139c5e28
codelive <dongfa@yeah.net>
2022-01-10 17:39:59 +0800

    support gitalk

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220110.1732';
+const cacheName = 'chirpy-20220110.1739';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
