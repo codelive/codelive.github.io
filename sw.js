ca6a0bdcdef480a04997c1f9b57b535febb60869
codelive <dongfa@yeah.net>
2022-01-10 12:28:32 +0800

    init blog

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220110.1218';
+const cacheName = 'chirpy-20220110.1227';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
