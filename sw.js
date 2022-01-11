050cfb5c76ec1696d9402e4a62c323e1248b21a1
codelive <dongfa@yeah.net>
2022-01-11 11:58:18 +0800

    remove share

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220110.1800';
+const cacheName = 'chirpy-20220111.1158';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
